fb40e7264fb64f4e31c1d4d72517e6e38c921797
Stefano <karlbovsky@gmail.com>
2017-06-09 13:41:23 +0200

    Fixed Twig Loader issue

--- Core/View.php
+++ Core/View.php
@@ -52,7 +52,7 @@ class View
          static $twig = null;
 
          if($twig===null){
-             $loader = new \Twig_loader_Filesystem(dirname(__DIR__).'/App/Views');
+             $loader = new \Twig_Loader_Filesystem(dirname(__DIR__).'/App/Views');
              $twig = new \Twig_Environment($loader);
             //  $twig->addGlobal('is_logged_in', \App\Auth::isLoggedIn());
              $twig->addGlobal('current_user', \App\Auth::getUser());
